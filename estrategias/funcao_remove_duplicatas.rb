c63aedcc64ce5985a01fd8af322ef648998f4e82
AnaLuizanc <analuizanc13@gmail.com>
2025-06-16 15:25:53 -0300

    Adiciona comentários de documentação

--- estrategias/funcao_remove_duplicatas.rb
+++ estrategias/funcao_remove_duplicatas.rb
@@ -1,3 +1,7 @@
+# Essa função faz a remoção dos valores duplicados de um vetor
+#
+# @param lista [Array<Integer>] vetor de inteiros
+# @return [Array<Integer>] vetor de inteiros sem duplicatas
 def remove_duplicatas lista
   mapa = Hash.new 0
   lista.each do |i|
