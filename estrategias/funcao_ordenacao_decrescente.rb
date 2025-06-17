30eb999871a178eb3fcfd320b21e6bf34df8c0c0
Davi Mendes <davis.sil@outlook.com>
2025-06-17 01:38:18 -0300

    Adicionada função de ordenação em ordem decrescente e atualizado o fluxo de execução do main.rb

---
+++ estrategias/funcao_ordenacao_decrescente.rb
@@ -0,0 +1,8 @@
+# Essa função faz a ordenação decrescente de um vetor
+#
+# @param lista [Array<Integer>] vetor de inteiros
+# @return [Array<Integer>] vetor de inteiros ordenado de forma decrescente
+
+def ordenacao_decrescente(lista)
+  lista.sort.reverse
+end
